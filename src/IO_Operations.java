69f32bf575e28e1a98a4f874d2bb498f2638d04e
Yuzo <contatovictoryuzo@gmail.com>
2026-02-26 19:29:23 -0300

    IO operations with Java.IO and Java.NIO

--- src/IO_Operations.java
+++ src/IO_Operations.java
@@ -1,9 +1,18 @@
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileTime;
+import java.nio.file.attribute.UserPrincipal;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class IO_Operations {
 
+    ///  using java IO
     public void createTXT() {
 
         String text = "Hello World \ntest";
@@ -41,6 +50,70 @@ public class IO_Operations {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+    ///  using java NIO
+    public void pathOperations() {
+
+        Path root = Path.of(System.getProperty("user.dir"));
+        Path filePath = root.resolve("file.txt");
+        System.out.println(root);
+        System.out.println(filePath);
+
+
+        boolean exists = Files.exists(filePath);
+        System.out.println(exists);
+
+        try {
+            FileTime lastModified = Files.getLastModifiedTime(filePath);
+            System.out.println(lastModified);
+        } catch (IOException ioException) {
+            throw  new RuntimeException(ioException);
+        }
+
+        try {
+            UserPrincipal owner = Files.getOwner(filePath);
+            System.out.println(owner);
+        } catch (IOException ioException) {
+            throw  new RuntimeException(ioException);
+        }
+
+        try {
+            Path newDir = Files.createDirectories(root.resolve("DirectoryCreatedByCommand"));
+
+            boolean fileExists = Files.exists(newDir.resolve("FileCreatedByCommand"));
+            if (!fileExists) {
+                Path newFile = Files.createFile(newDir.resolve("FileCreatedByCommand"));
+                Files.write(newFile, "test \n test \n third line".getBytes());
+            }
+
+        } catch (IOException ioException) {
+            throw  new RuntimeException(ioException);
+        }
+
+        try {
+
+            List<String> lines = Files.readAllLines(root.resolve("DirectoryCreatedByCommand/FileCreatedByCommand"));
+            for (String line : lines) {
+                System.out.println(line);
+            }
+
+        } catch (IOException ioException) {
+            throw  new RuntimeException(ioException);
+        }
+
+        try (Stream<String> lines = Files.lines(root.resolve("DirectoryCreatedByCommand/FileCreatedByCommand"))) {
+
+            lines.filter(l -> l.contains("test")).forEach(System.out::println);
+
+        } catch (IOException ioException) {
+            throw  new RuntimeException(ioException);
+        }
+
+
+
+
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         IO_Operations ioOperations = new IO_Operations();
-        ioOperations.createTXT();
+        ioOperations.pathOperations();
 
 
 
